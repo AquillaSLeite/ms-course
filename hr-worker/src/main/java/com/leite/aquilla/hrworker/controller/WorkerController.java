ce072cf9b08aee6877b165a1efac4c02db67d91d
Aquilla Silva Leite <aquilla11@hotmail.com>
2021-05-08 15:25:31 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/leite/aquilla/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/leite/aquilla/hrworker/controller/WorkerController.java
@@ -2,7 +2,10 @@ package com.leite.aquilla.hrworker.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,11 @@ import com.leite.aquilla.hrworker.service.WorkerService;
 @RestController
 @RequestMapping("/workers")
 public class WorkerController {
+
+	private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+	@Value("${test.config}")
+	private String testConfig;
 
 	WorkerService workerService;
 
@@ -31,6 +39,7 @@ public class WorkerController {
 
 	@GetMapping("{id}")
 	public ResponseEntity<Worker> show(@PathVariable Long id) {
+		logger.info("CONFIG = " + this.testConfig);
 		return ResponseEntity.ok().body(workerService.findById(id));
 	}
 
