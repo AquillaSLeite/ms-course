22e28d67c183fbbbc0290483ab2b3d5b44755ab5
Aquilla Silva Leite <aquilla11@hotmail.com>
2021-05-08 14:51:26 -0300

    configuring hr-config-server project

---
+++ hr-config-server/src/test/java/leite/aquilla/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package leite.aquilla.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
