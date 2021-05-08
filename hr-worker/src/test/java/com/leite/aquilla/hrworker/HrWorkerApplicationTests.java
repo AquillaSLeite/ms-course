245a798a8cc8f1571602ab46712123d8ff5126cd
Aquilla Silva Leite <aquilla11@hotmail.com>
2021-05-08 10:56:07 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/leite/aquilla/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leite.aquilla.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
